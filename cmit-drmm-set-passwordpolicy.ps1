33c084924730fd68df6bbcf7f1052580bdbbb0a0
dyerseve <dyerseve@gmail.com>
2025-02-21 13:19:17 -0500

    Update cmit-drmm-set-passwordpolicy.ps1

--- cmit-drmm-set-passwordpolicy.ps1
+++ cmit-drmm-set-passwordpolicy.ps1
@@ -1,40 +1,78 @@
 <# cmit-drmm-set-passwordpolicy.ps1
 pellis@cmitsolutions.com
-2025-01-07-001
+2025-02-20-001
 
 Creates entries for password policy.
 Schedule this weekly to ensure it is re-enforced if changed locally.
 
 #>
 
-# Define variables for the settings
-$complexity = $env:complexity # Boolean, set to 1 to enforce password complexity, 0 to disable
-$minAge = $env:minAge # String, Minimum password age in days
-$maxAge = $env:maxAge # String, Maximum password age in days
-$minLength = $env:minLength # String, Minimum password length
-$history = $env:history # String, Password history size
+<# Recommended options:
+$PasswordComplexity = 1
+$MinimumPasswordLength = 12
+$MaximumPasswordAge = 90
+$EnableAdminAccount = 0
+$EnableGuestAccount = 0
+$ClearTextPassword = 0
+#>
 
-# Enforce password complexity
-$complexityKey = "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters"
-$complexityValueName = "PasswordComplexity"
-Set-ItemProperty -Path $complexityKey -Name $complexityValueName -Value $complexity -Type DWord
+$PasswordComplexity = $env:drmmPasswordComplexity
+$MinimumPasswordLength = $env:drmmMinimumPasswordLength
+$MaximumPasswordAge = $env:drmmMaximumPasswordAge
+$EnableAdminAccount = $env:drmmEnableAdminAccount
+$EnableGuestAccount = $env:drmmEnableGuestAccount
+$ClearTextPassword = $env:drmmClearTextPassword
 
-# Set minimum password age
-$minAgeKey = "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters"
-$minAgeValueName = "MinimumPasswordAge"
-Set-ItemProperty -Path $minAgeKey -Name $minAgeValueName -Value $minAge -Type DWord
+write-host PasswordComplexity: $env:drmmPasswordComplexity
+write-host MinimumPasswordLength: $env:drmmMinimumPasswordLength
+write-host MaximumPasswordAge: $env:MaximumPasswordAge
+write-host EnableAdminAccount: $env:drmmEnableAdminAccount
+write-host EnableGuestAccount: $env:drmmEnableGuestAccount
+write-host ClearTextPassword: $env:drmmClearTextPassword
 
-# Set maximum password age
-$maxAgeKey = "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters"
-$maxAgeValueName = "MaximumPasswordAge"
-Set-ItemProperty -Path $maxAgeKey -Name $maxAgeValueName -Value $maxAge -Type DWord
+<# Testing #>
+Function Parse-SecPol($CfgFile){ 
+    secedit /export /cfg "$CfgFile" | out-null
+    $obj = New-Object psobject
+    $index = 0
+    $contents = Get-Content $CfgFile -raw
+    [regex]::Matches($contents,"(?<=\[)(.*)(?=\])") | %{
+        $title = $_
+        [regex]::Matches($contents,"(?<=\]).*?((?=\[)|(\Z))", [System.Text.RegularExpressions.RegexOptions]::Singleline)[$index] | %{
+            $section = new-object psobject
+            $_.value -split "\r\n" | ?{$_.length -gt 0} | %{
+                $value = [regex]::Match($_,"(?<=\=).*").value
+                $name = [regex]::Match($_,".*(?=\=)").value
+                $section | add-member -MemberType NoteProperty -Name $name.tostring().trim() -Value $value.tostring().trim() -ErrorAction SilentlyContinue | out-null
+            }
+            $obj | Add-Member -MemberType NoteProperty -Name $title -Value $section
+        }
+        $index += 1
+    }
+    return $obj
+}
 
-# Enforce password history
-$historyKey = "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters"
-$historyValueName = "PasswordHistorySize"
-Set-ItemProperty -Path $historyKey -Name $historyValueName -Value $history -Type DWord
+Function Set-SecPol($Object, $CfgFile){
+   $SecPool.psobject.Properties.GetEnumerator() | %{
+        "[$($_.Name)]"
+        $_.Value | %{
+            $_.psobject.Properties.GetEnumerator() | %{
+                "$($_.Name)=$($_.Value)"
+            }
+        }
+    } | out-file $CfgFile -ErrorAction Stop
+    secedit /configure /db c:\windows\security\local.sdb /cfg "$CfgFile" /areas SECURITYPOLICY
+}
 
-# Enforce minimum password length
-$minLengthKey = "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters"
-$minLengthValueName = "MinimumPasswordLength"
-Set-ItemProperty -Path $minLengthKey -Name $minLengthValueName -Value $minLength -Type DWord
+$SecPool = Parse-SecPol -CfgFile C:\Temp.cfg
+$SecPool.'System Access'.PasswordComplexity = $PasswordComplexity
+$SecPool.'System Access'.MinimumPasswordLength = $MinimumPasswordLength
+$SecPool.'System Access'.MaximumPasswordAge = $MaximumPasswordAge
+$SecPool.'System Access'.EnableAdminAccount = $EnableAdminAccount
+$SecPool.'System Access'.EnableGuestAccount = $EnableGuestAccount
+$SecPool.'System Access'.ClearTextPassword = $ClearTextPassword
+
+
+Set-SecPol -Object $SecPool -CfgFile C:\Temp.cfg
+Remove-Item C:\Temp.cfg -Force
+
